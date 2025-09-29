f87df511b80f158b3fbcf0730e0ca690c8b9a505
Marjorie <aubertmarjorie2@gmail.com>
2025-09-29 14:20:56 +0200

    wip pipelines

---
+++ sandbox/packages/pipelines.ts
@@ -0,0 +1,13 @@
+//Effects are Immutable
+
+//1. Pipe
+
+//2. Map
+
+//Syntax
+
+//const mappedEffect = pipe(myEffect, Effect.map(transformation))
+// or
+//const mappedEffect = Effect.map(myEffect, transformation)
+// or
+//const mappedEffect = myEffect.pipe(Effect.map(transformation))
